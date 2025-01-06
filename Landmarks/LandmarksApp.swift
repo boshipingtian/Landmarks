1c3aed7d9bda9f3bea346e996207a53a2b0120d5
MrArchie <1619794359@qq.com>
2025-01-06 23:46:39 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 段瑞卿 on 2025/1/6.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by 段瑞卿 on 2025/1/6.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
