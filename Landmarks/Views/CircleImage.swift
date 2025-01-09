7ff65748b4a745f522e4159f804988ce3a41d29d
MrArchie <1619794359@qq.com>
2025-01-10 00:42:11 +0800

    first commit

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 段瑞卿 on 2025/1/9.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 段瑞卿 on 2025/1/10.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    var coordinate: CLLocationCoordinate2D
+    
+    var body: some View {
+        Map(position: .constant(.region(region)))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+}
